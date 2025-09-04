3318e9ddb941e78aacdbce2fc656a3e47c8a5ec5
Daria <rafflezyana@gmail.com>
2025-09-04 10:05:10 +0200

    added modal window and js

---
+++ js/modal.js
@@ -0,0 +1,12 @@
+const modal = document.querySelector(".backdrop");
+
+const modalBtnOpen = document.querySelector(".modal-btn-open");
+const modalBtnClose = document.querySelector(".modal-btn-close");
+
+const toggleModal = () => modal.classList.toggle("is-hidden");
+
+modalBtnOpen.addEventListener("click", toggleModal);
+modalBtnClose.addEventListener("click", toggleModal);
+
+modalBtnOpen.addEventListener("click", toggleModal);
+modalBtnClose.addEventListener("click", toggleModal);
